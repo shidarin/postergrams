8dab7e54ffb2fc9888ab3207bef81d43c1fa9052
Sean Wallitsch <shidarin@alphamatte.com>
2014-10-28 09:34:52 -0700

    Removes socialists prefix by placing url patterns in main postergrams urls.py file

--- postergrams/urls.py
+++ postergrams/urls.py
@@ -1,12 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from socialists import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'postergrams.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^socialists/', include('socialists.urls')),
+    # /
+    url(r'^$', views.index, name='index'),
+    # /artists/OllyMoss/
+    url(r'^artists/(?P<artist_name>[\w\d]+)/$', views.artist, name='artist'),
+    # /galleries/Mondo/
+    url(r'^galleries/(?P<gallery_name>[\w\d]+)/$', views.gallery, name='gallery'),
+    # /events/Batman75/
+    url(r'^events/(?P<event_name>[\w\d]+)/$', views.event, name='event'),
+    # /admin/
     url(r'^admin/', include(admin.site.urls)),
 )
 
