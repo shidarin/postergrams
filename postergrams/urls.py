96f5f50bd0e2e7d0994e3f79fae7e9ff825b0050
Sean Wallitsch <shidarin@alphamatte.com>
2014-11-01 16:24:14 -0700

    Getting started on templates

--- postergrams/urls.py
+++ postergrams/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'postergrams.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^socialists/', include('socialists.urls')),
+    url(r'^', include('socialists.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
 
